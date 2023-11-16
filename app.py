f14b30ae3589b792949e6b2a6b43605625280724
tirmina <148883484+tirmina@users.noreply.github.com>
2023-11-16 20:45:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -15,5 +15,22 @@ def process():
         command = f'ffmpeg -i "{video_url}" -ss {start_time} -to {end_time} output_audio.mp3'
         subprocess.run(command, shell=True)
         return "Conversion successful! <a href='/'>Back</a>"
+
+import os
+import requests
+
+@app.route('/download_thumbnail', methods=['POST'])
+def download_thumbnail():
+    video_url = request.form['video_url']
+    thumbnail_url = f'http://img.youtube.com/vi/{video_url.split("v=")[1]}/0.jpg'
+    thumbnail = requests.get(thumbnail_url)
+
+    if thumbnail.status_code == 200:
+        with open('thumbnail.jpg', 'wb') as file:
+            file.write(thumbnail.content)
+        return "Thumbnail downloaded successfully! <a href='/'>Back</a>"
+    else:
+        return "Thumbnail download failed. <a href='/'>Back</a>"
+
     
     # Include handling for other functionalities like thumbnail download
